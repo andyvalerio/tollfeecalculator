b789e333ad873984c05648c544566c9004d15923
andyvandermeyde <giuseppe@valerio.nu>
2022-01-27 19:09:23 +0100

    Commit empty springboot project

---
+++ src/main/java/org/te/toll/TollFeeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.te.toll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollFeeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollFeeCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/te/toll/TollFeeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.te.toll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollFeeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
